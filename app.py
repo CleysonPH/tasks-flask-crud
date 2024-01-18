ffb2b597d1f19a92fc94638eae3a5f8c549b0d73
Cleyson <cleysonph@gmail.com>
2024-01-18 11:46:25 -0300

    separate logic of the routes

--- app.py
+++ app.py
@@ -6,13 +6,16 @@ app = Flask(__name__)
 task_repository = TaskRepository()
 
 
-@app.route("/tasks", methods=("POST", "GET"))
+@app.route("/tasks", methods=("POST",))
 def create_task():
-    if request.method == "GET":
-        tasks = [task.to_dict() for task in task_repository.get_all()]
-        return {"tasks": tasks, "total_tasks": len(tasks)}, 200
     data = request.get_json()
     task = task_repository.create(
         title=data.get("title"), description=data.get("description")
     )
     return task.to_dict(), 201
+
+
+@app.route("/tasks", methods=("GET",))
+def get_tasks():
+    tasks = [task.to_dict() for task in task_repository.get_all()]
+    return {"tasks": tasks, "total_tasks": len(tasks)}, 200
